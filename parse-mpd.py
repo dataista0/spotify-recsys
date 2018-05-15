bf50af1debe68184d416c22ef95ab7f7e94b6594
Julian Peller <jpeller@navent.com>
2018-05-15 17:42:23 +0000

    put __name__=='__main__' in parse-mpd.py

--- parse-mpd.py
+++ parse-mpd.py
@@ -1,4 +1,3 @@
-
 import os
 import json
 import pandas as pd
@@ -14,7 +13,6 @@ MAPS = {'track': {},
        }
 
 TRACK_INFO = {}
-
 
 def reduce_playlist(playlist):
     global MAPS, COUNTERS, TRACK_INFO
@@ -42,17 +40,21 @@ def reduce_playlist(playlist):
         res['tracks'].append(track['track_uri'])
     return res
 
+def run():
+    files = [(i, f, BASEPATH + f) for i, f in enumerate(os.listdir(BASEPATH)) if f.endswith(".json")]
+    print(len(files))
 
-
-files = [(i, f, BASEPATH + f) for i, f in enumerate(os.listdir(BASEPATH)) if f.endswith(".json")]
-print(len(files))
-
-
-for index, basename, fullpath in files:
-    print("Processing {} ({})".format(index, basename))
-    playlists = json.load(open(fullpath))['playlists']
-    reduced_playlists =  [reduce_playlist(pl) for pl in playlists]
-    json.dump(reduced_playlists, open(BASEPATH+"reduced/{}".format(basename), "w"))
+    for index, basename, fullpath in files:
+        print("Processing {} ({})".format(index, basename))
+        playlists = json.load(open(fullpath))['playlists']
+        reduced_playlists =  [reduce_playlist(pl) for pl in playlists]
+        json.dump(reduced_playlists, open(BASEPATH+"reduced/{}".format(basename), "w"))
     
-json.dump(MAPS, open(BASEPATH+"reduced/maps.json", "w"))
-json.dump(TRACK_INFO, open(BASEPATH+"reduced/track_info.json", "w"))
+    json.dump(MAPS, open(BASEPATH+"reduced/maps.json", "w"))
+    json.dump(TRACK_INFO, open(BASEPATH+"reduced/track_info.json", "w"))
+    
+if __name__ == '__main__':
+    run()
+
+    
+
